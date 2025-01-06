a44766f2108a575a82cb544686b709328ed77526
choe <0410kj@naver.com>
2025-01-07 00:37:56 +0900

    first

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Test-1
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
