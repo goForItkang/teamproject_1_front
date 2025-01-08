63b0001fd8ba0ad212ed1d5e9d956bb46caaf638
goForItkang <kangyohan.dev.0421@gmail.com>
2025-01-08 13:11:24 +0900

    rechecking

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Header from "./component/Header";
 import Main from "./page/Main";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Chat from "./page/Chat";
+import Login from "./page/Login";
+import Signup from "./page/Signup";
+import Shop from "./page/Shop";
 
 function App() {
   return (
@@ -12,10 +16,10 @@ function App() {
             <Routes>
                 <Route>
                     <Route path="/" element={<Main />} />
-                    <Route path={"/chat"}/>
-                    <Route path={"/login"}/>
-                    <Route path={"/signup"}/>
-                    <Route path={"/shop"}/>
+                    <Route path={"/chat"} element={<Chat/>}/>
+                    <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/signup"} element={<Signup/>}/>
+                    <Route path={"/shop"} element={<Shop/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
