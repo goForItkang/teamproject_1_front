b563a7febec0089260ffc8e059b35ce8066c27f9
goForItkang <kangyohan.dev.0421@gmail.com>
2025-01-08 12:30:05 +0900

    yh

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./component/Header";
 import Main from "./page/Main";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header></Header>
-        <Main></Main>
+        <Header></Header>
+        <BrowserRouter>
+            <Routes>
+                <Route>
+                    <Route path="/" element={<Main />} />
+                    <Route path={"/chat"}/>
+                    <Route path={"/login"}/>
+                    <Route path={"/signup"}/>
+                    <Route path={"/shop"}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
